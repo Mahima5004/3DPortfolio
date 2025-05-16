cfdf350abc5aa8a9581a2d60e0379d222cca3b15
Mahima <mahimamishra5008@gmail.com>
2025-05-16 22:50:53 +0530

    Implement About page by adding Vertical timeline in the experience box

---
+++ src/constants/index.js
@@ -0,0 +1,183 @@
+import { meta, shopify, starbucks, tesla } from "../assets/images";
+import {
+    car,
+    contact,
+    css,
+    estate,
+    express,
+    git,
+    github,
+    html,
+    javascript,
+    linkedin,
+    mongodb,
+    nodejs,
+    pricewise,
+    react,
+    redux,
+    sass,
+    snapgram,
+    summiz,
+    tailwindcss,
+    threads,
+} from "../assets/icons";
+
+export const skills = [
+    {
+        imageUrl: css,
+        name: "CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: express,
+        name: "Express",
+        type: "Backend",
+    },
+    {
+        imageUrl: git,
+        name: "Git",
+        type: "Version Control",
+    },
+    {
+        imageUrl: github,
+        name: "GitHub",
+        type: "Version Control",
+    },
+    {
+        imageUrl: html,
+        name: "HTML",
+        type: "Frontend",
+    },
+    {
+        imageUrl: javascript,
+        name: "JavaScript",
+        type: "Frontend",
+    },
+    {
+        imageUrl: mongodb,
+        name: "MongoDB",
+        type: "Database",
+    },
+    
+   
+    {
+        imageUrl: nodejs,
+        name: "Node.js",
+        type: "Backend",
+    },
+    {
+        imageUrl: react,
+        name: "React",
+        type: "Frontend",
+    },
+    {
+        imageUrl: redux,
+        name: "Redux",
+        type: "State Management",
+    },
+    {
+        imageUrl: sass,
+        name: "Sass",
+        type: "Frontend",
+    },
+    {
+        imageUrl: tailwindcss,
+        name: "Tailwind CSS",
+        type: "Frontend",
+    },
+    
+];
+
+export const experiences = [
+    {
+        title: "Software Engineer",
+        company_name: "Jio Platforms Limited",
+        icon: starbucks,
+        iconBg: "#accbe1",
+        date: "December 2023 - April 2024",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    {
+        title: "Software Developer",
+        company_name: "Jio Platforms Limited",
+        icon: tesla,
+        iconBg: "#fbc3bc",
+        date: "April 2024 - Present",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    
+    
+];
+
+export const socialLinks = [
+    {
+        name: 'Contact',
+        iconUrl: contact,
+        link: '/contact',
+    },
+    {
+        name: 'GitHub',
+        iconUrl: github,
+        link: 'https://github.com/Mahima5004',
+    },
+    {
+        name: 'LinkedIn',
+        iconUrl: linkedin,
+        link: 'https://www.linkedin.com/in/mishramahima/',
+    }
+];
+
+export const projects = [
+    {
+        iconUrl: pricewise,
+        theme: 'btn-back-red',
+        name: 'Disease Predictor',
+        description: 'Developed a web application that tracks and notifies users of price changes for products on Amazon, helping users find the best deals.',
+        link: 'https://github.com/Mahima5004/DiseasePredictor',
+    },
+    {
+        iconUrl: threads,
+        theme: 'btn-back-green',
+        name: 'Jerry The Voice Bot',
+        description: 'Created a full-stack replica of the popular discussion platform "Threads," enabling users to post and engage in threaded conversations.',
+        link: 'https://github.com/Mahima5004/Jerry-The-Voice-Bot',
+    },
+    {   
+        iconUrl: car,
+        theme: 'btn-back-blue',
+        name: 'Car Finding App',
+        description: 'Designed and built a mobile app for finding and comparing cars on the market, streamlining the car-buying process.',
+        link: 'https://github.com/adrianhajdin/project_next13_car_showcase',
+    },
+    {
+        iconUrl: snapgram,
+        theme: 'btn-back-pink',
+        name: 'MemoryGame',
+        description: 'Built a complete clone of Instagram, allowing users to share photos and connect with friends in a familiar social media environment.',
+        link: 'https://github.com/Mahima5004/MemoryGame',
+    },
+    {
+        iconUrl: estate,
+        theme: 'btn-back-black',
+        name: 'Drum Kit',
+        description: 'Developed a web application for real estate listings, facilitating property searches and connecting buyers with sellers.',
+        link: 'https://github.com/Mahima5004/DrumKit',
+    },
+    {
+        iconUrl: summiz,
+        theme: 'btn-back-yellow',
+        name: 'The Bid Game',
+        description: 'App that leverages AI to automatically generate concise & informative summaries from lengthy text content, or blogs.',
+        link: 'https://github.com/Mahima5004/react_bidGame',
+    }
+];
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,113 @@
 import React from 'react'
+import { skills, experiences } from '../constants'
+import {VerticalTimeline, VerticalTimelineElement} from 'react-vertical-timeline-component'
+import 'react-vertical-timeline-component/style.min.css'
+
+import CTA from '../components/CTA';
+
 
 function About() {
   return (
-    <div>About</div>
+    <section className='max-container'>
+        <h1 className='head-text'>
+          Hello! I'm <span className='blue-gradient-text font-semibold drop-shadow'>
+          Mahima</span>
+        </h1>
+
+        <div className='mt-5 flex flex-col gap-3 text-slate-500'>
+          <p>
+            Software Engineer based in India, specializing in 
+            building (and occassionally designing) exceptional
+            digital experiences.
+          </p>
+        </div>
+
+        <div className='py-10 flex flex-col'>
+          <h3 className='subhead-text'>My Skills</h3>
+
+          <div className='mt-16 flex flex-wrap gap-12'>
+            {skills.map((skill, index) => (
+              <div  key = {index} className='block-container w-20 h-20'>
+                <div className='btn-back rounded-xl'/>
+                <div className='btn-front rounded-xl flex justify-center items-center'>
+                  <img 
+                  src= {skill.imageUrl} 
+                  alt={skill.name} 
+                  className='w-1/2 h-1/2 object-contain'
+                  />
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        {/* Experiance */}
+        <div className='py-16'>
+          <h3 className='subhead-text'>Work Experiance</h3>
+          <div className='mt-5 flex flex-col gap-3 text-slate-500'>
+             <p>
+            Software Engineer based in India, specializing in 
+            building (and occassionally designing) exceptional
+            digital experiences.
+          </p>
+          </div>
+
+          <div className='mt-12 flex'></div>
+            <VerticalTimeline>
+              {experiences.map((experience, index) => (
+                <VerticalTimelineElement
+                 key={experience.company_name}
+                 date={experience.date}
+                 icon={
+                 <div className='flex justify-center items-center
+                  w-full h-full'>
+                  <img 
+                  src= {experience.icon} 
+                  alt={experience.company_name}
+                  className='w-[60%] h-[60%] object-contain' 
+                  />
+                 </div>
+                 }
+                  
+                 iconStyle = {
+                  {
+                    background: experience.iconBg,
+                    // boxShadow: 'none',
+                  }
+                 }
+                 contentStyle={
+                  {
+                    borderBottom : '8px',
+                    borderStyle : 'solid',
+                    borderBottomColor : experience.iconBg,
+                    boxShadow : 'none',
+                  }
+                 }
+                >
+                  <div key={index}>
+                    <h3 className='text-black text-xl font-poppins font-semibold'>
+                      {experience.title}
+                    </h3>
+                    <p className='text-black-500 font-medium font-base'
+                      style={{ margin: 0 }}>
+                      {experience.company_name}
+                    </p>
+                  </div>
+                  <ul className='my-5 list-disc ml-5 space-y-2'>
+                    {experience.points.map((point, index) => (
+                      <li key={`experience-point-${index}`} className='text-black-500/50 pl-1 text-sm font-normal'>
+                        {point}
+                      </li>
+                    ))}
+                  </ul>
+                </VerticalTimelineElement>
+              ))}
+            </VerticalTimeline>
+        </div>
+
+        <hr className='border-slate-200'/>
+        <CTA />
+    </section>
   )
 }
 
