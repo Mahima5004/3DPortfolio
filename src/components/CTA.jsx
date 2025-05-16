ba3a70822c1845f06d82b91b094963e7c6d630bd
Mahima <mahimamishra5008@gmail.com>
2025-05-16 23:01:01 +0530

    Added contact section in the footer of the About page

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function CTA() {
   return (
-    <div>CTA</div>
+    <section className='cta'>
+     <p className='cta-text'>Have a project in mind? <br className='sm:block hidden'/>
+     Let's build something amazing together!</p>
+     
+    <Link to = '/contact' className = 'btn'>
+      Contact Me
+    </Link>
+    </section>
   )
 }
 
